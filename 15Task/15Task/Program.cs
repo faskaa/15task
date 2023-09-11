da5acb5c86f177c476b79e8dd9a9c89bf94f704b
faskaa <fazilnb@code.edu.az>
2023-09-11 17:55:38 +0400

    first commit

---
+++ 15Task/15Task/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _15Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //File.Create(@"C:\Users\acer\Desktop\mytest.txt");
+
+            //task2
+            //File.Delete(@"C:\Users\acer\Desktop\mytest.txt")
+
+            //task3
+            //File.Create(@"C:\Users\acer\Desktop\mytest.txt");
+            //try
+            //{
+            //    File.Create(@"C:\Users\acer\Desktop\mytest.txt");
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine("The file already exsist!");
+            //}
+
+            //task4
+            //string pathName = @"C:\Users\acer\Desktop\mytest.txt";
+            //FileStream fs = File.Create(pathName);
+            //fs.Close();
+            //File.WriteAllText(pathName, "hello");
+
+            //task5
+            //string pathName = @"C:\Users\acer\Desktop\mytest.txt";
+            //FileStream fs = File.Create(pathName);
+            //fs.Close();
+            //File.WriteAllText(pathName, "hello");
+            //Console.WriteLine(File.ReadAllText(pathName));
+
+            //task6
+            //string pathName = @"C:\Users\acer\Desktop\mytest.txt";
+            //FileStream fs = File.Create(pathName);
+            //fs.Close();
+            //string[] strings = { "line1", "line2" };
+            //File.WriteAllLines(pathName,strings);
+
+            //task7
+            //task8
+            //string pathName = @"C:\Users\acer\Desktop\mytest.txt";
+            //FileStream fs = File.Create(pathName);
+            //fs.Close();
+            //File.Create(pathName);
+            //File.WriteAllText(pathName, " salam");
+            //fs.Close();
+            //File.WriteAllText(pathName, "sagol ");
+
+
+
+
+
+
+        }
+    }
+}
